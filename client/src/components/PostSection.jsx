9e3d87da311d12a99705a4f3cededa35a6bed56a
dlwldma <carlosv20x@gmail.com>
2022-11-03 12:06:26 -0300

    dfsfdsfds

--- client/src/components/PostSection.jsx
+++ client/src/components/PostSection.jsx
@@ -8,10 +8,10 @@ const PostSection = () => {
 
     const getData = async () =>{
         //DEVELOPMENT
-        const response = await axios.get('http://localhost:4000/api');
+        /* const response = await axios.get('http://localhost:4000/api'); */
 
         //DEPLOYMENT
-        /* const response = await axios.get('https://tell-app.herokuapp.com/api'); */
+        const response = await axios.get('https://tell-app.herokuapp.com/api');
         const data = await response.data;
         return data;
     } 
